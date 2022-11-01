0dca955e5a4b7776b409c35e59b2538e6b66e908
Carmyo <carmelodicristina@gmail.com>
2022-11-01 13:53:36 +0100

    create class

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+class Guinzagli {
+    public $taglia;
+    public $colore;
+}
+?>
+
+<?php
+class Trasportini {
+    public $colore;
+    public $dimensioni;
+    public $ruote
+}
+?>
+<?php
+class Giochi {
+    public $palle;
+    public $intelligenza;
+    public $peluche;
+}
+?>
+
+<?php
+class Gabbie{
+    public $grandezza;
+    public $accessoriata;
+}
+?>
+
+<?php
+class Cibo {
+    public $animale;
+    public $peso;
+    public $tipo;
+}
+?>
