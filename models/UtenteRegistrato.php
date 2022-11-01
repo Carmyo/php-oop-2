66bf31883a30f4f15eb12de5019b7d9e6a42e991
Carmyo <carmelodicristina@gmail.com>
2022-11-01 14:41:38 +0100

    create function to do an order

--- models/UtenteRegistrato.php
+++ models/UtenteRegistrato.php
@@ -4,5 +4,10 @@ class UtenteRegistrato extends User {
     public $name;
     public $surname;
     private $password;
+    public $carrello = [];
+
+    public function addOrder($prodotto){
+        $this->carrello[] = $prodotto;
+     }
 }
 ?>
